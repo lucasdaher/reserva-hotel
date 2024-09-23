d73956f69112e7d97c838c62d40a75625313d36a
lucasdaher <contato@lucasdaher.com>
2024-09-22 23:42:58 -0300

    refatorando funções...

--- src/hotel/controllers/QuartoController.java
+++ src/hotel/controllers/QuartoController.java
@@ -2,3 +2,4 @@ package hotel.controllers;
 
 public class QuartoController {
 }
+
--- src/hotel/controllers/ReservaController.java
+++ src/hotel/controllers/ReservaController.java
@@ -1,12 +1,4 @@
 package hotel.controllers;
 
-import hotel.models.Hospede;
-
 public class ReservaController {
-    public boolean criarReserva(String numQuarto, String nomeHospede) {
-        Hospede hospede = new Hospede();
-        hospede.setNome(nomeHospede);
-        return false;
-    }
-
 }
