7e31efa5b52b9b618d2bfb9122c01703bb5c4321
lucasdaher <contato@lucasdaher.com>
2024-09-27 01:54:13 -0300

    criando uma função separada para cadastrar quartos iniciais.

--- src/hotel/controllers/QuartoController.java
+++ src/hotel/controllers/QuartoController.java
@@ -11,6 +11,23 @@ public class QuartoController {
         return quartosDisponiveis;
     }
 
+    public void cadastrarQuartos(QuartoController quartoController) {
+        quartoController.adicionarQuarto(new Quarto(101, "Standard", 250.0));
+        quartoController.adicionarQuarto(new Quarto(102, "Standard", 250.0));
+        quartoController.adicionarQuarto(new Quarto(103, "Luxury", 150.0));
+        quartoController.adicionarQuarto(new Quarto(104, "Luxury", 150.0));
+
+        quartoController.adicionarQuarto(new Quarto(201, "Standard", 250.0));
+        quartoController.adicionarQuarto(new Quarto(202, "Standard", 250.0));
+        quartoController.adicionarQuarto(new Quarto(203, "Luxury", 150.0));
+        quartoController.adicionarQuarto(new Quarto(204, "Luxury", 150.0));
+
+        quartoController.adicionarQuarto(new Quarto(301, "Standard", 250.0));
+        quartoController.adicionarQuarto(new Quarto(302, "Standard", 250.0));
+        quartoController.adicionarQuarto(new Quarto(303, "Luxury", 150.0));
+        quartoController.adicionarQuarto(new Quarto(304, "Luxury", 150.0));
+    }
+
     public void adicionarQuarto(Quarto quarto) {
         quartosDisponiveis.add(quarto);
     }
--- src/hotel/views/ConsoleView.java
+++ src/hotel/views/ConsoleView.java
@@ -2,7 +2,6 @@ package hotel.views;
 
 import hotel.controllers.QuartoController;
 import hotel.controllers.ReservaController;
-import hotel.models.Quarto;
 
 import java.util.Scanner;
 
@@ -11,20 +10,7 @@ public class ConsoleView {
         QuartoController quartoController = new QuartoController();
         ReservaController reservaController = new ReservaController(quartoController);
 
-        quartoController.adicionarQuarto(new Quarto(101, "Standard", 250.0));
-        quartoController.adicionarQuarto(new Quarto(102, "Standard", 250.0));
-        quartoController.adicionarQuarto(new Quarto(103, "Luxury", 150.0));
-        quartoController.adicionarQuarto(new Quarto(104, "Luxury", 150.0));
-
-        quartoController.adicionarQuarto(new Quarto(201, "Standard", 250.0));
-        quartoController.adicionarQuarto(new Quarto(202, "Standard", 250.0));
-        quartoController.adicionarQuarto(new Quarto(203, "Luxury", 150.0));
-        quartoController.adicionarQuarto(new Quarto(204, "Luxury", 150.0));
-
-        quartoController.adicionarQuarto(new Quarto(301, "Standard", 250.0));
-        quartoController.adicionarQuarto(new Quarto(302, "Standard", 250.0));
-        quartoController.adicionarQuarto(new Quarto(303, "Luxury", 150.0));
-        quartoController.adicionarQuarto(new Quarto(304, "Luxury", 150.0));
+        quartoController.cadastrarQuartos(quartoController);
 
         Scanner scanner = new Scanner(System.in);
         int opcao;
